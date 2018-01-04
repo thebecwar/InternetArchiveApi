3d632ffc3225a31c2d2bcf1f055c9bedc92e349c
Mark Becwar <mark@thebecwar.com>
2018-01-04 09:55:14 -0500

    Parens Not Curlies
    I'm an idiot.

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
+@echo off
 REM Borrowed from https://weblogs.asp.net/bsimser/automatically-publishing-nuget-packages-from-github
 
-@echo off
 set config=%1
 if "%config%" == "" (
     set config=Release
 )
 
 set version=1.0.0
-if not "%PackageVersion%" = "" {
+if not "%PackageVersion%" = "" (
     set version=%PackageVersion%
-}
+)
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild InternetArchiveApi.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%MsBuildExe% InternetArchiveApi.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
