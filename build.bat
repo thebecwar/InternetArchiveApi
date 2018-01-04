14496903688205d25352f33906b8379db161101f
Mark Becwar <mark@thebecwar.com>
2018-01-04 09:49:07 -0500

    remove dead line in build batch

--- build.bat
+++ build.bat
@@ -11,11 +11,6 @@ if not "%PackageVersion%" = "" {
     set version=%PackageVersion%
 }
 
-set nuget=
-if "%nuget%" == "" (
-    set nuget=nuget
-)
-
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild InternetArchiveApi.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
